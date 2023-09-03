22c5ed6e440cf8134e54587b29f166eae6baedce
Abdullah <105864626+OneAbdullah@users.noreply.github.com>
2023-09-03 12:26:48 +0300

    Autumate git install

--- deploy_website.sh
+++ deploy_website.sh
@@ -16,7 +16,6 @@ while true; do
     httpd_service_status=""
     repo_cloned=false
     html_found=false
-    pip_installed=false
 
     # Function to check if a command was successful
     check_success() {
@@ -34,10 +33,10 @@ while true; do
     httpd_installed=true
 
     # Install httpd
-    echo "Installing pip..."
-    yum install -y pip
+    echo "Installing git..."
+    yum install -y git
     check_success
-    pip_installed=true
+    git_installed=true
 
 
     # Enable and start Apache2
@@ -47,26 +46,30 @@ while true; do
     check_success
     httpd_service_status=$(systemctl is-active httpd)
 
-    # Clone the repository from GitHub
+ # Clone the repository from GitHub
     echo "Cloning the repository from GitHub..."
-    wget -q -N "$repo_url"
+    git clone "$repo_url"
     check_success
     repo_cloned=true
 
     # Check for .htm or .html files
     echo "Checking for .htm or .html files..."
-html_files=$(find . -type f ( -name ".htm" -o -name ".html" ))
-if [ -n "$html_files" ]; then
-    html_found=true
-    echo "Found the following HTML/HTM files:"
-    select file in $html_files; do
-        mv "$file" "$web_dir/index.html"
-        check_success
-        break
-    done
-else
-    echo "No .htm or .html files found."
-fi
+    html_files=$(find . -type f \( -name "*.htm" -o -name "*.html" \))
+    if [ -n "$html_files" ]; then
+        html_found=true
+        echo "Found the following HTML/HTM files:"
+        select file in $html_files; do
+            if [ -n "$file" ]; then
+                mv "$file" "$web_dir/index.html"
+                check_success
+                break
+            else
+                echo "Invalid choice. Please select a valid file."
+            fi
+        done
+    else
+        echo "No .htm or .html files found."
+    fi
 
     # Print the public IP address of the server
     public_ip=$(curl -s https://ipinfo.io/ip)
@@ -79,7 +82,7 @@ fi
     echo "Step 4: Check for .htm or .html files - $html_found"
 
     # Print a final message
-    if [ "$httpd_installed" = true ] && [ "$httpd_service_status" = "active" ] && [ "$repo_cloned" = true ] && ["$pip_installed" = true]; then
+    if [ "$httpd_installed" = true ] && [ "$httpd_service_status" = "active" ] && [ "$repo_cloned" = true ]; then
         echo "Website deployment successful!"
 
 
